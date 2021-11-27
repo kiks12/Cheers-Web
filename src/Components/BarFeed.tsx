5808f0ecda1d69bf6d633f3d0e252d6c8b14c84a
Francis James Tolentino <francistolentino1107@gmail.com>
2021-11-27 22:54:52 +0800

    BarFeed Component done

---
+++ src/Components/BarFeed.tsx
@@ -0,0 +1,35 @@
+import {MdStar, MdOutlineLocationOn} from "react-icons/md";
+
+const BarFeed = () => {
+    return(
+        <div className="mt-4 h-28 bg-white border rounded-lg p-2 flex justify-between">
+            <div className="flex flex-col flex-1 pr-2">
+                <div className="border-b">
+                    <div className="flex items-center">
+                        <p className="font-semibold text-lg mr-2">Bar Name</p>
+                        <div className="flex">
+                            <MdStar color="#FFE769" size={20} />
+                            <MdStar color="#FFE769" size={20} />
+                            <MdStar color="#FFE769" size={20} />
+                            <MdStar color="#FFE769" size={20} />
+                            <MdStar color="#FFE769" size={20} />
+                        </div>
+                    </div>
+                    <p className="text-md">Bar</p>
+                </div>
+                <div className="flex items-center h-full">
+                    <MdOutlineLocationOn size={19} color="gray"/>
+                    <p className="text-gray-400 ml-1">1.2 km</p>
+                    <p className="text-gray-400 ml-10">200php - 500php</p>
+                </div>
+            </div>
+
+            <div className="w-48 h-full bg-black rounded-md text-white flex items-center justify-center">
+                image
+            </div>
+
+        </div>
+    )
+}
+
+export default BarFeed;
--- src/Components/Feed.tsx
+++ src/Components/Feed.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import BarFeed from "./BarFeed";
 import Filter from "./Filter";
 import SortBy from "./SortBy";
 
 const Feed = () => {
+    const [bars, setBars] = useState(() => [0,1,2,3,4,5,6,7,8,9]);
+
     return(
         <div className="mt-10">
             <Filter />
             <SortBy />
-            <p>sfdfsfs</p>
+            {
+                bars && bars.map(() => {
+                    return <BarFeed />
+                })
+            }
         </div>
     )
 }
