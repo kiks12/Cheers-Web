6ff1b2f9e07c0c671285dcf02dc8cde63a003498
Francis James Tolentino <francistolentino1107@gmail.com>
2021-11-30 23:30:47 +0800

    main html tag responsiveness fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() : JSX.Element {
       <title>Home</title>
     </Head>
     <Navhar />
-    <main className="lg:container md:container sm:mx-5 lg:mx-auto md:mx-auto h-screen py-16 flex flex-col items-center">
+    <main className="lg:container md:mx-5 sm:mx-5 lg:mx-auto h-screen py-16 flex flex-col items-center">
       <Featured />
       <Feed />
     </main>
