94b3de35965b63a247c69a2acb0a1aa13ec30cda
Francis James Tolentino <francistolentino1107@gmail.com>
2021-11-30 23:48:50 +0800

    custom hook useInnerWidth onload event listener unmount added

--- src/Custom-Hooks/useInnerWidth.tsx
+++ src/Custom-Hooks/useInnerWidth.tsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from "react";
 
 const getWindowInnerWidth = (setInitialState : React.Dispatch<React.SetStateAction<number>>) => {
-    setInitialState((prev: number) => prev = window.innerWidth);
+    setInitialState((prev) => prev = window.innerWidth);
 }
 
 const useInnerWidth = () => {
     const [intialState, setInitialState] = useState<number>(0);
 
     useEffect(() => {
-        window.addEventListener("load", () => {
-            console.log("load");
-            setInitialState((prev: number) => prev = window.innerWidth);
-        })
-        
+        window.addEventListener("load", getWindowInnerWidth.bind(null, setInitialState));
+        return () => window.removeEventListener("load", getWindowInnerWidth.bind(null, setInitialState));
     }, []);
     
     useEffect(() => {
         window.addEventListener("resize", getWindowInnerWidth.bind(null, setInitialState));
-
-        return () => {
-            window.removeEventListener("resize", getWindowInnerWidth.bind(null, setInitialState));
-        }
+        return () => window.removeEventListener("resize", getWindowInnerWidth.bind(null, setInitialState));
     }, []);
 
     return [intialState];
