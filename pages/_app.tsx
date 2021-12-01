d75a4c030b53caaca87e51750746c7ac245a4f62
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-01 11:31:32 +0800

    poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/out.css";
 import "../styles/globals.css";
+import Head from "next/head";
 
 interface myAppProps {
   Component: any;
@@ -7,7 +8,14 @@ interface myAppProps {
 }
 
 const MyApp = ({ Component, pageProps } : myAppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"/>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/Components/Feed/Featured.tsx
+++ src/Components/Feed/Featured.tsx
@@ -5,39 +5,41 @@ import Link from "next/dist/client/link";
 const Featured : React.FC = () => {
 
     return(
-        <Link href="page/Bar-name/overview" >
-            <div className="w-full flex flex-col items-center cursor-pointer">
+        <>
+            <div className="w-full flex flex-col items-center">
                 <div className="w-full">
                     <p className="font-light">Featured by Cheers</p>
                 </div>
 
-                <div className="h-72 w-full rounded-2xl p-4 flex justify-between mt-1 shadow-lg hover:w-full-hover hover:shadow-2xl hover:h-80 transition-all relative featured"
-                    style={{
-                        backgroundImage: "url(bar-goto-nyc-bar-interior-Alex-lau.jpeg)",
-                        backgroundSize: "contain",
-                    }}>
-                    
-                    <div className="flex flex-col justify-start z-10">
-                        <h1 className="text-white font-bold text-6xl">Bar Name</h1>
+                <Link href="page/Bar-name/overview">
+                    <div className="cursor-pointer h-72 w-full rounded-2xl p-4 flex justify-between mt-1 shadow-lg hover:w-full-hover hover:shadow-2xl hover:h-80 transition-all relative featured"
+                        style={{
+                            backgroundImage: "url(bar-goto-nyc-bar-interior-Alex-lau.jpeg)",
+                            backgroundSize: "contain",
+                        }}>
+                        
+                        <div className="flex flex-col justify-start z-10">
+                            <h1 className="text-white font-bold text-6xl">Bar Name</h1>
 
-                        <div className="flex items-center mt-1">
-                            <p className="font-light text-white mr-2">Reviews</p>
-                            <div className="flex">
-                                <MdStar color="yellow" size={20}/>
-                                <MdStar color="yellow" size={20}/>
-                                <MdStar color="yellow" size={20}/>
-                                <MdStar color="yellow" size={20}/>
-                                <MdStar color="yellow" size={20}/>
+                            <div className="flex items-center mt-1">
+                                <p className="font-light text-white mr-2">Reviews</p>
+                                <div className="flex">
+                                    <MdStar color="yellow" size={20}/>
+                                    <MdStar color="yellow" size={20}/>
+                                    <MdStar color="yellow" size={20}/>
+                                    <MdStar color="yellow" size={20}/>
+                                    <MdStar color="yellow" size={20}/>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="w-1/4 h-full bg-white rounded-xl items-center justify-center z-10 sm:hidden lg:flex md:flex">
-                        Map
+                        <div className="w-1/4 h-full bg-white rounded-xl items-center justify-center z-10 sm:hidden lg:flex md:flex">
+                            Map
+                        </div>
                     </div>
-                </div>
+                </Link>
             </div>
-        </Link>
+        </>
     )
     
 }
