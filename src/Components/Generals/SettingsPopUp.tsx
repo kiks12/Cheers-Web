891d79b681764ef572bd92f0c03324b417744481
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-01 22:08:17 +0800

    settings pop up working

--- src/Components/Generals/SettingsPopUp.tsx
+++ src/Components/Generals/SettingsPopUp.tsx
@@ -1,8 +1,23 @@
-const SettingsPopUp = () => {
+import Link from "next/link";
+import React from "react";
+
+interface SettingsPopUpProps {}
+
+const SettingsPopUp: React.FC<SettingsPopUpProps> = () => {
   return (
-    <>
-      <p>asdfasdf</p>
-    </>
+    <div
+      className="absolute w-48 h-auto border p-2 rounded-md shadow-lg"
+      style={{ top: "1.2rem", right: "0.1rem", backgroundColor: "#ffff" }}
+    >
+      <button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all">
+        Account Settings
+      </button>
+      <Link href="/login">
+        <button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all">
+          Log Out
+        </button>
+      </Link>
+    </div>
   );
 };
 
--- src/Components/Generals/navbar.tsx
+++ src/Components/Generals/navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MdOutlineHome,
   MdCalendarToday,
@@ -7,6 +7,7 @@ import {
 } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
+import SettingsPopUp from "./SettingsPopUp";
 
 interface NavBarProps {
   activePage?: string;
@@ -17,72 +18,82 @@ const ACTIVE_NAVBAR_LINK_STYLING =
 const INACTIVE_NAVBAR_LINK_STYLING =
   "w-1/6 border-b-4 border-transparent h-full flex items-center justify-center cursor-pointer hover:bg-white";
 
+const handleSettingsPopUpClick = (
+  setShowSettingsPopUp: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  setShowSettingsPopUp((prev) => (prev = !prev));
+};
+
 const Navhar: React.FC<NavBarProps> = ({ activePage }) => {
+  const [showSettingsPopUp, setShowSettingsPopUp] = useState<boolean>(false);
+
   return (
-    <>
-      <nav
-        className="flex w-full h-12 px-5 items-center border-b justify-between fixed z-20"
-        style={{ backgroundColor: "white" }}
-      >
-        <h2 className="text-2xl font-semibold">Cheers.</h2>
+    <nav
+      className="flex w-full h-12 px-5 items-center border-b justify-between fixed z-20"
+      style={{ backgroundColor: "white" }}
+    >
+      <h2 className="text-2xl font-semibold">Cheers.</h2>
 
-        <div className="w-1/4 lg:block md:block sm:hidden h-full items-center justify-center p-2">
-          <div className="w-full h-full bg-white border rounded-full pl-3 flex pt-1 pr-1 pb-1">
-            <input
-              className="w-full bg-transparent h-full focus:outline-none text-sm"
-              placeholder="Search"
-            />
-            <div className="bg-white w-8 h-full rounded-full cursor-pointer hover:bg-gray-200 flex items-center justify-center">
-              <MdSearch size={20} />
-            </div>
+      <div className="w-1/4 lg:block md:block sm:hidden h-full items-center justify-center p-2">
+        <div className="w-full h-full bg-white border rounded-full pl-3 flex pt-1 pr-1 pb-1">
+          <input
+            className="w-full bg-transparent h-full focus:outline-none text-sm"
+            placeholder="Search"
+          />
+          <div className="bg-white w-8 h-full rounded-full cursor-pointer hover:bg-gray-200 flex items-center justify-center">
+            <MdSearch size={20} />
           </div>
         </div>
+      </div>
 
-        <ul className="lg:flex md:flex sm:hidden justify-start w-1/4 h-full">
-          <Link href="/">
-            {activePage === "home" ? (
-              <li className={ACTIVE_NAVBAR_LINK_STYLING}>
-                <MdOutlineHome size={24} />
-              </li>
-            ) : (
-              <li className={INACTIVE_NAVBAR_LINK_STYLING}>
-                <MdOutlineHome size={24} />
-              </li>
-            )}
-          </Link>
+      <ul className="lg:flex md:flex sm:hidden justify-start w-1/4 h-full">
+        <Link href="/">
+          {activePage === "home" ? (
+            <li className={ACTIVE_NAVBAR_LINK_STYLING}>
+              <MdOutlineHome size={24} />
+            </li>
+          ) : (
+            <li className={INACTIVE_NAVBAR_LINK_STYLING}>
+              <MdOutlineHome size={24} />
+            </li>
+          )}
+        </Link>
 
-          <Link href="/profile/reservations">
-            {activePage === "reservations" ? (
-              <li className={ACTIVE_NAVBAR_LINK_STYLING}>
-                <MdCalendarToday size={19} />
-              </li>
-            ) : (
-              <li className={INACTIVE_NAVBAR_LINK_STYLING}>
-                <MdCalendarToday size={19} />
-              </li>
-            )}
-          </Link>
-        </ul>
+        <Link href="/profile/reservations">
+          {activePage === "reservations" ? (
+            <li className={ACTIVE_NAVBAR_LINK_STYLING}>
+              <MdCalendarToday size={19} />
+            </li>
+          ) : (
+            <li className={INACTIVE_NAVBAR_LINK_STYLING}>
+              <MdCalendarToday size={19} />
+            </li>
+          )}
+        </Link>
+      </ul>
 
-        <div className="items-center lg:flex md:flex sm:hidden">
-          <div className="rounded-full h-8 w-8 bg-black mr-2">
-            <span>
-              <i></i>
-            </span>
-          </div>
-          <p className="text-sm">Username</p>
-          <div className="flex items-center">
-            <div className="ml-3 w-6 h-6 cursor-pointer rounded-full hover:bg-gray-200">
-              <MdArrowDropDown size={24} />
-            </div>
-          </div>
+      <div className="items-center lg:flex md:flex sm:hidden">
+        <div className="rounded-full h-8 w-8 bg-black mr-2">
+          <span>
+            <i></i>
+          </span>
         </div>
-
-        <div className="lg:hidden md:hidden sm:block">
-          <GiHamburgerMenu size={24} />
+        <p className="text-sm">Username</p>
+        <div className="flex items-center relative">
+          <div
+            className="ml-3 w-6 h-6 cursor-pointer rounded-full hover:bg-gray-200 transition-all"
+            onClick={() => handleSettingsPopUpClick(setShowSettingsPopUp)}
+          >
+            <MdArrowDropDown size={24} />
+          </div>
+          {showSettingsPopUp && <SettingsPopUp />}
         </div>
-      </nav>
-    </>
+      </div>
+
+      <div className="lg:hidden md:hidden sm:block">
+        <GiHamburgerMenu size={24} />
+      </div>
+    </nav>
   );
 };
 
