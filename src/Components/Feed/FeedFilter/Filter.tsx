62335595a36d736b0d364c0f511cba7ec2948a4a
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-01 11:44:26 +0800

    responsiveness optimized

--- src/Components/Feed/FeedFilter/Filter.tsx
+++ src/Components/Feed/FeedFilter/Filter.tsx
@@ -1,5 +1,4 @@
 import React, { MutableRefObject, useEffect, useRef, useState } from "react";
-import useInnerWidth from "../../../Custom-Hooks/useInnerWidth";
 import FilterComponent from "./FilterComponent";
 import FilterModal from "./FilterModal";
 
@@ -22,7 +21,6 @@ const getFilterContainerPosition = (elementRef: MutableRefObject<HTMLDivElement 
 const Filter : React.FC = () => {
     const filterContainer = useRef<HTMLDivElement | null>(null);
     const [showFilterModal, setShowFilterModal] = useState<boolean>(false);
-    const [innerWidth] = useInnerWidth();
 
     useEffect(() => {
         window.addEventListener("scroll", () => getFilterContainerPosition(filterContainer));
@@ -35,30 +33,22 @@ const Filter : React.FC = () => {
 
     return(
         <>
-            { 
-                innerWidth >= 1280 && 
-                <div ref={filterContainer} className="w-screen sticky flex items-center justify-center" style={{top: "3rem"}}>
-                    <div className="flex lg:items-end mt-1 pb-5 pt-2 lg:container md:container sm:mx-5 lg:flex-row lg:justify-between md:flex-col md:w-full md:items-center md:justify-center sm:flex-col sm:w-full sm:items-center sm:justify-center">
-                        <FilterComponent showCancel={false}/> 
-                    </div>
+            <div ref={filterContainer} className="w-screen sticky items-center justify-center lg:flex md:hidden sm:hidden" style={{top: "3rem"}}>
+                <div className="flex lg:items-end mt-1 pb-5 pt-2 lg:container md:container sm:mx-5 lg:flex-row lg:justify-between md:flex-col md:w-full md:items-center md:justify-center sm:flex-col sm:w-full sm:items-center sm:justify-center">
+                    <FilterComponent showCancel={false}/> 
                 </div>
-            }
+            </div>
 
-            {
-                (innerWidth < 1280) &&
-                <div className="w-full flex items-center justify-center">
-                    <button className="border border-black w-full py-2 px-5 rounded-md hover:bg-black hover:text-white transition-all"
-                        onClick={() => {
-                            setShowFilterModal(true);
-                        }}>
-                        Filters
-                    </button>
-                </div>
-            }
+            <div className="w-full flex items-center justify-center lg:hidden md:block">
+                <button className="border border-black w-full py-2 px-5 rounded-md hover:bg-black hover:text-white transition-all"
+                    onClick={() => {
+                        setShowFilterModal(true);
+                    }}>
+                    Filters
+                </button>
+            </div>
 
-            {
-                (showFilterModal && innerWidth < 1280) && <FilterModal setShowFilterModal={setShowFilterModal}/>
-            }
+            <FilterModal showFilterModal={showFilterModal} setShowFilterModal={setShowFilterModal}/>
         </>
     )
 }
