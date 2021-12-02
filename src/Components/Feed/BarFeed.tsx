0186a2e8bba6b0212071eff6f8215ea2760679cc
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-02 23:30:59 +0800

    bar feed responsiveness

--- src/Components/Feed/BarFeed.tsx
+++ src/Components/Feed/BarFeed.tsx
@@ -3,7 +3,7 @@ import { MdStar, MdOutlineLocationOn } from "react-icons/md";
 
 const BarFeed: React.FC = () => {
 	return (
-		<div className="mt-4 h-28 bg-white border rounded-lg p-2 flex justify-between w-full">
+		<div className="mt-4 h-auto bg-white border rounded-lg p-2 flex justify-between w-full">
 			<div className="flex flex-col flex-1 pr-2">
 				<div className="border-b">
 					<div className="flex items-center">
@@ -18,14 +18,14 @@ const BarFeed: React.FC = () => {
 					</div>
 					<p className="text-md">Bar</p>
 				</div>
-				<div className="flex items-center h-full">
+				<div className="flex items-center h-full py-1">
 					<MdOutlineLocationOn size={19} color="gray" />
 					<p className="text-gray-400 ml-1">1.2 km</p>
 					<p className="text-gray-400 ml-10">200php - 500php</p>
 				</div>
 			</div>
 
-			<div className="w-48 h-full bg-black rounded-md text-white flex items-center justify-center">
+			<div className="w-48 h-full bg-black rounded-md text-white flex items-center justify-center lg:flex md:flex sm:hidden">
 				image
 			</div>
 		</div>
