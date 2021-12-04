9a66454a74592ce1e7d2d7490be520d44582fdfa
kiks12 <francistolentino1107@gmail.com>
2021-12-04 08:51:20 +0800

    filter component fonts small

--- src/Components/Feed/FeedFilter/FilterComponent.tsx
+++ src/Components/Feed/FeedFilter/FilterComponent.tsx
@@ -10,20 +10,20 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
 	setShowFilterModal,
 }) => {
 	return (
-		<div className="flex flex-1 lg:flex-row md:flex-col md:w-full sm:flex-col sm:w-full">
+		<div className="flex flex-1 lg:flex-row md:flex-col md:w-full sm:flex-col sm:w-full justify-between">
 			<div className="flex lg:flex-row lg:items-end lg:justify-between md:flex-col md:w-full sm:flex-col sm:w-full">
-				<div className="flex-1 lg:mr-5 md:mr-0 sm:mr-0 ">
+				<div className="flex-1 lg:mr-5 md:mr-0 sm:mr-0 flex flex-col justify-between h-full">
 					<p className="font-semibold mb-2">Type</p>
-					<select className="w-full h-10 bg-white border rounded-md px-2">
+					<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
 						<option>Both</option>
 						<option>Bars</option>
 						<option>Clubs</option>
 					</select>
 				</div>
 
-				<div className="flex-1 lg:mr-5 md:mr-0">
+				<div className="flex-1 lg:mr-5 md:mr-0 flex flex-col justify-between h-full">
 					<p className="font-semibold mb-2">Reviews</p>
-					<select className="w-full h-10 bg-white border rounded-md px-2">
+					<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
 						<option>All</option>
 						<option>5 Stars</option>
 						<option>4 Stars</option>
@@ -33,9 +33,9 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
 					</select>
 				</div>
 
-				<div className="flex-1 lg:mr-5 md:mr-0">
+				<div className="flex-1 lg:mr-5 md:mr-0 flex flex-col justify-between h-full">
 					<p className="font-semibold mb-2">Location</p>
-					<select className="w-full h-10 bg-white border rounded-md px-2">
+					<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
 						<option>Near Me</option>
 						<option>Bars</option>
 						<option>Clubs</option>
@@ -54,8 +54,8 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
 				</div>
 			</div>
 
-			<div className="flex lg:w-1/4 md:w-full sm:w-full lg:pl-5 items-center justify-center md:mt-10 sm:mt-10">
-				<button className="w-full bg-black text-white font-light h-10 rounded-md hover:bg-gray-700 transition-all">
+			<div className="flex lg:w-1/4 md:w-full sm:w-full lg:pl-5 items-end justify-end md:mt-10 sm:mt-10">
+				<button className="w-2/3 bg-black text-white font-light h-8 rounded-md hover:bg-gray-700 transition-all text-sm">
 					Apply
 				</button>
 			</div>
