3f379e2ada2463ac36e0d78f5711104cdb4318c4
kiks12 <francistolentino1107@gmail.com>
2021-12-04 09:23:31 +0800

    sort by font small

--- src/Components/Feed/SortBy.tsx
+++ src/Components/Feed/SortBy.tsx
@@ -4,7 +4,7 @@ const SortBy: React.FC = () => {
 	return (
 		<div className="mt-10 w-full">
 			<p className="mb-2">Sort By:</p>
-			<select className="h-10 w-48 px-2 bg-white border rounded-md">
+			<select className="h-8 w-48 px-2 bg-white border rounded-md text-sm">
 				<option>Location</option>
 				<option>Reviews</option>
 			</select>
