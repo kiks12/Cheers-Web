b611f53de01af6742b6cc5aa110f164927471353
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-06 00:03:42 +0800

    links

--- src/Components/Generals/PopUpSettings/SettingsPopUp.tsx
+++ src/Components/Generals/PopUpSettings/SettingsPopUp.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import useCursorOutside from "../../../Custom-Hooks/useCursorOutside";
 
 interface SettingsPopUpProps {
@@ -19,16 +19,16 @@ const SettingsPopUp: React.FC<SettingsPopUpProps> = ({ setShowSettingsPopUp }) =
 			className="absolute w-48 h-auto border p-2 rounded-md shadow-lg lg:top-5 md:top-5 sm:top-10"
 			style={{ right: "0.1rem", backgroundColor: "#ffff" }}
 		>
-			{/* <Link href="/profile/settings"> */}
-			<button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all">
-				Account Settings
-			</button>
-			{/* </Link> */}
-			{/* <Link href="/login"> */}
-			<button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all">
-				Log Out
-			</button>
-			{/* </Link> */}
+			<Link href="/profile/settings">
+				<button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all">
+					Account Settings
+				</button>
+			</Link>
+			<Link href="/login">
+				<button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all">
+					Log Out
+				</button>
+			</Link>
 		</div>
 	);
 };
