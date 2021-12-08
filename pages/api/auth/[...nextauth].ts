e73c1cc46a9b611b1e74f28cc4a6563a1e8f8985
kiks12 <francistolentino1107@gmail.com>
2021-12-08 14:16:21 +0800

    secret for production

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
+    secret: "YxD0B2pHi7YmneR7LewlR6XaRhUr/gm+0bxUy87jC1A=",
     providers:[
         GoogleProvider({
             clientId: "440043100008-d4m6dvh53v3jhk7i2lpuf5cr2j1s45ce.apps.googleusercontent.com",
