d7ce65f330f9f859eb2bcf99e6f02132ceea1fef
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-09 19:16:23 +0800

    reservations

--- pages/profile/reservations/index.tsx
+++ pages/profile/reservations/index.tsx
@@ -1,40 +1,48 @@
 import { GetStaticProps } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 import React from "react";
 import ReservationFeed from "../../../src/Components/Reservations/ReservationFeed";
 import ReservationsHeader from "../../../src/Components/Reservations/ReservationsHeader";
 
 interface ReservationsProps {
-  reservations: Array<any>;
+	reservations: Array<any>;
 }
 
 const Reservations: React.FC<ReservationsProps> = ({ reservations }) => {
-  return (
-    <>
-      <Head>
-        <title>Reservations</title>
-      </Head>
-      {/* <Navhar activePage="reservations" /> */}
-      <main className='lg:container lg:mx-auto md:mx-5 sm:mx-5 py-16'>
-        <ReservationsHeader />
-        {reservations.length !== 0 ? (
-          reservations.map((reservation) => {
-            return <ReservationFeed key={reservation} />;
-          })
-        ) : (
-          <p>No Reservations to Show</p>
-        )}
-      </main>
-    </>
-  );
+	const { data: session } = useSession();
+
+	return (
+		<>
+			<Head>
+				<title>Reservations</title>
+			</Head>
+			<main className="lg:container lg:mx-auto md:mx-5 sm:mx-5 py-16">
+				{session && <ReservationsHeader />}
+				{reservations.length !== 0 && session ? (
+					reservations.map((reservation) => {
+						return <ReservationFeed key={reservation} />;
+					})
+				) : (
+					<div className="w-full flex items-center flex-col">
+						<p>You need to sign up to see reservations</p>
+						<Link href="/login">
+							<p className="cursor-pointer">Log In</p>
+						</Link>
+					</div>
+				)}
+			</main>
+		</>
+	);
 };
 
 export const getStaticProps: GetStaticProps = () => {
-  return {
-    props: {
-      reservations: [1, 2, 3, 4, 5],
-    },
-  };
+	return {
+		props: {
+			reservations: [1, 2, 3, 4, 5],
+		},
+	};
 };
 
 export default Reservations;
