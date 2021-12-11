d896966f19d73e699b528b8c4a71516aecce2520
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-11 23:16:19 +0800

    useActivePageHook better

--- src/Custom-Hooks/useActivePage.tsx
+++ src/Custom-Hooks/useActivePage.tsx
@@ -1,14 +1,29 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const activePageContext = createContext<any>([]);
 
 export const ActivePageProvider = ({ children }: any) => {
-  const [activePage, setActivePage] = useState<any>("home");
-  return (
-    <activePageContext.Provider value={[activePage, setActivePage]}>
-      {children}
-    </activePageContext.Provider>
-  );
+	const router = useRouter();
+
+	const [activePage, setActivePage] = useState<any>(null);
+
+	useEffect(() => {
+		const path = router.pathname;
+		switch (path) {
+			case "/":
+				setActivePage("home");
+				break;
+			case "/profile/reservations":
+				setActivePage("reservations");
+				break;
+			default:
+				setActivePage("");
+				break;
+		}
+	}, [router, setActivePage]);
+
+	return <activePageContext.Provider value={[activePage, setActivePage]}>{children}</activePageContext.Provider>;
 };
 
 export const useActivePage = () => useContext(activePageContext);
