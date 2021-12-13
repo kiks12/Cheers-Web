d157aaed9b03189216c9586d608e8f8fb2943694
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-13 10:15:31 +0800

    file structure refactored

--- src/Components/Header/layout.tsx
+++ src/Components/Header/layout.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useActivePage } from "../../Custom-Hooks/useActivePage";
-import Navhar from "../Generals/Navbar/navbar";
+import Navhar from "./Navbar/navbar";
 
 const Layout: React.FC = ({ children }) => {
-  const [activePage, setActivePage] = useActivePage();
-  return (
-    <div>
-      <Navhar activePage={activePage} setActivePage={setActivePage} />
-      {children}
-    </div>
-  );
+	const [activePage, setActivePage] = useActivePage();
+	return (
+		<div>
+			<Navhar activePage={activePage} setActivePage={setActivePage} />
+			{children}
+		</div>
+	);
 };
 
 export default Layout;
