f5bcde9e00be113feed99d2991b97393a4d03878
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-13 10:22:52 +0800

    footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,26 @@ import Featured from "../src/Components/Feed/Featured";
 import Feed from "../src/Components/Feed/Feed";
 import { NextPage } from "next";
 import Router from "next/router";
+import Footer from "../src/Components/Footer/Footer";
 
 const Home: NextPage = () => {
-  Router.events.on("routeChangeStart", () => {
-    console.log("route changing");
-  });
+	Router.events.on("routeChangeStart", () => {
+		console.log("route changing");
+	});
 
-  return (
-    <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      {/* <Navhar activePage='home' /> */}
-      <main className='lg:container md:mx-5 sm:mx-5 lg:mx-auto h-full py-16 flex flex-col items-center'>
-        <Featured />
-        <Feed />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Home</title>
+			</Head>
+			{/* <Navhar activePage='home' /> */}
+			<main className="lg:container md:mx-5 sm:mx-5 lg:mx-auto h-full py-16 flex flex-col items-center">
+				<Featured />
+				<Feed />
+			</main>
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="h-96 bg-white border-t-1 border p-3">
+			<div className="container mx-auto flex items-center justify-center">
+				<p className="text-4xl">Footer</p>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
