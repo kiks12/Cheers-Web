57c76b7b4869ff18e78969013c5abe9532b9e9f8
kiks12 <francistolentino1107@gmail.com>
2021-12-16 08:30:06 +0800

    navbar flicker fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,31 @@ import Head from "next/head";
 import Featured from "../src/Components/Feed/Featured";
 import Feed from "../src/Components/Feed/Feed";
 import { NextPage } from "next";
-import Router from "next/router";
-import Footer from "../src/Components/Footer/Footer";
 import useTopOfPage from "../src/Custom-Hooks/useTopOfPage";
 import ScrollToTopButton from "../src/Components/ScrollToTop/ScrollToTopButton";
+import { useSession } from "next-auth/react";
+import Footer from "../src/Components/Footer/Footer";
 
 const Home: NextPage = () => {
-	const topOfPage = useTopOfPage();
-	Router.events.on("routeChangeStart", () => {
-		console.log("route changing");
-	});
-
-	return (
-		<>
-			<Head>
-				<title>Home</title>
-			</Head>
-			{/* <Navhar activePage='home' /> */}
-			<main className="lg:container md:mx-5 sm:mx-5 lg:mx-auto h-full py-16 flex flex-col items-center">
-				<Featured />
-				<Feed />
-				{topOfPage !== 0 && <ScrollToTopButton />}
-			</main>
-			<Footer />
-		</>
-	);
+  const topOfPage = useTopOfPage();
+  const { status } = useSession();
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      {status !== "loading" && (
+        <>
+          <main className='lg:container md:mx-5 sm:mx-5 lg:mx-auto h-full py-16 flex flex-col items-center'>
+            <Featured />
+            <Feed />
+            {topOfPage !== 0 && <ScrollToTopButton />}
+          </main>
+          <Footer />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
