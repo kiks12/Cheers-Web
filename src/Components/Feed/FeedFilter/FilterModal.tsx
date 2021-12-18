eaf1e7dfd454b99da853687cfe65e190bb48ba23
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-18 23:52:14 +0800

    filter modal z-index set to 40

--- src/Components/Feed/FeedFilter/FilterModal.tsx
+++ src/Components/Feed/FeedFilter/FilterModal.tsx
@@ -14,7 +14,7 @@ const FilterModal: React.FC<FilterModalProps> = ({ setShowFilterModal }) => {
 	const filterModalRef = useCursorOutside(() => handleFilterModalClose(setShowFilterModal));
 
 	return (
-		<div className="fixed w-screen h-screen bg-black top-0 z-20 bg-opacity-60 px-10 py-10 items-center justify-center transition-all flex">
+		<div className="fixed w-screen h-screen bg-black top-0 z-20 bg-opacity-60 px-10 py-10 items-center justify-center transition-all flex z-50">
 			<div className="md:w-1/2 sm:w-full h-full bg-white rounded-md border flex flex-col items-center justify-between p-5" ref={filterModalRef}>
 				<h1 className="text-xl font-semibold h-24">Filters</h1>
 				<FilterComponent setShowFilterModal={setShowFilterModal} showCancel={true} />
