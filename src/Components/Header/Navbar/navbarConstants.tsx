5c4fc582222eeca82e3c224acc45b25fc054e6be
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 21:10:41 +0800

    Pop up Settings Refactors

--- src/Components/Header/Navbar/navbarConstants.tsx
+++ src/Components/Header/Navbar/navbarConstants.tsx
@@ -17,6 +17,9 @@ type LargeScreenActiveSettings = 0;
 type SmallScreenActiveSettings = 1;
 
 
+type SignoutCallbackUrl = "http://localhost:3000/login";
+
+
 export const ACTIVE_NAVBAR_LINK_STYLING : ActiveNavbarLink=
 	"lg:w-1/5 md:w-1/4 border-b-4 border-black h-full flex items-center justify-center cursor-pointer hover:bg-white";
 
@@ -26,3 +29,6 @@ export const INACTIVE_NAVBAR_LINK_STYLING : InactiveNavbarLink =
 
 export const LARGE_SCREEN_ACTIVE_SETTINGS : LargeScreenActiveSettings = 0;
 export const SMALL_SCREEN_ACTIVE_SETTINGS : SmallScreenActiveSettings = 1;
+
+
+export const SIGNOUT_CALLBACK_URL : SignoutCallbackUrl = "http://localhost:3000/login";
--- src/Components/Header/PopUpSettings/SettingsPopUp.tsx
+++ src/Components/Header/PopUpSettings/SettingsPopUp.tsx
@@ -1,17 +1,30 @@
+
+/*
+
+Cheers - Pop Up Settings in Navigation Bar
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
 /* eslint-disable @next/next/link-passhref */
 import Link from "next/link";
 import React from "react";
 import useCursorOutside from "../../../Custom-Hooks/useCursorOutside";
-import {signOut} from "next-auth/react";
+import { signOut } from "next-auth/react";
+
+import { SIGNOUT_CALLBACK_URL } from "../Navbar/navbarConstants";
 
 interface SettingsPopUpProps {
 	setShowSettingsPopUp: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const SettingsPopUp: React.FC<SettingsPopUpProps> = ({ setShowSettingsPopUp }) => {
+
 	const mainRef = useCursorOutside(() => {
 		setShowSettingsPopUp((prev) => (prev = !prev));
 	});
+
 
 	return (
 		<div
@@ -19,15 +32,19 @@ const SettingsPopUp: React.FC<SettingsPopUpProps> = ({ setShowSettingsPopUp }) =
 			className="absolute w-48 h-auto border p-2 rounded-md shadow-lg lg:top-5 md:top-5 sm:top-10"
 			style={{ right: "0.1rem", backgroundColor: "#ffff" }}
 		>
+			
 			<Link href="/profile/settings">
 				<button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all">
 					Account Settings
 				</button>
 			</Link>
-			<button className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all"
-			onClick={() => signOut({callbackUrl: "http://localhost:3000/login"})}>
+
+			<button 
+				className="py-2 px-5 text-sm flex items-center justify-center w-full hover:bg-gray-100 active:bg-gray-200 transition-all"
+				onClick={() => signOut({callbackUrl: SIGNOUT_CALLBACK_URL})}>
 				Log Out
 			</button>
+
 		</div>
 	);
 };
