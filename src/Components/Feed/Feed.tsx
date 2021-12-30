ccbd9c493340f3d70dd921dec33888ab1bc157b9
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 22:04:50 +0800

    Feed Component refactored

--- src/Components/Feed/Feed.tsx
+++ src/Components/Feed/Feed.tsx
@@ -1,19 +1,41 @@
+
+/*
+
+Cheers - Feed Component
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
+
 import React, { useState } from "react";
+
+
 import BarFeed from "./BarFeed";
 import Filter from "./FeedFilter/Filter";
 import SortBy from "./SortBy";
 
+
 const Feed: React.FC = () => {
+
   const [bars] = useState(() => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
   return (
     <div className='mt-10 w-full flex flex-col items-center'>
+      
       <Filter />
+
       <SortBy />
-      {bars &&
-        bars.map((bar) => {
-          return <BarFeed key={bar} />;
-        })}
+      
+      {
+        bars ? 
+          bars.map((bar) => {
+            return <BarFeed key={bar} />;
+          }) : (
+            <p>No Bars to show</p>
+          )
+      }
+
     </div>
   );
 };
