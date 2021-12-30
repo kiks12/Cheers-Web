829be40710643e6c8d7ee233dea8cc07c98684d0
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 21:23:44 +0800

    Footer commit

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,18 @@
+
+/*
+
+Cheers - Footer Component
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
+
 import React from "react";
 
+
 const Footer = () => {
+	
 	return (
 		<div className="h-96 bg-white border-t-1 border p-3">
 			<div className="container mx-auto flex items-center justify-center">
