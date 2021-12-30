0e884ebdb98463ee0a86b1e5954a80a63fd8871a
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 21:33:29 +0800

    useCursorOutside hook cleaned

--- src/Custom-Hooks/useCursorOutside.tsx
+++ src/Custom-Hooks/useCursorOutside.tsx
@@ -1,11 +1,22 @@
+
+/*
+
+Cheers - custom hook for closing pop up components on click outside itself
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
 import React, { useEffect, useRef } from "react";
 
 type ElementRefType = HTMLDivElement | null;
 
 const useCursorOutside = (handler: () => void, cursorRef?: React.MutableRefObject<ElementRefType>) => {
+	
 	const elementRef = typeof cursorRef !== "undefined" ? cursorRef : useRef<ElementRefType>(null);
 
 	useEffect(() => {
+		
 		const mainHandler = (e: any) => {
 			if (elementRef.current && !elementRef.current.contains(e.target)) {
 				handler();
