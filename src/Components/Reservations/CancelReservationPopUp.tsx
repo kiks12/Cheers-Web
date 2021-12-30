d5f6b3a4529775df6107d82ff597a2ffb62470d0
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 22:24:19 +0800

    cancel reservation pop up code cleaned

--- src/Components/Reservations/CancelReservationPopUp.tsx
+++ src/Components/Reservations/CancelReservationPopUp.tsx
@@ -1,4 +1,16 @@
-import React, { useRef } from "react";
+
+/*
+
+Cheers - Pop Up Cancel reservation Component
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
+
+import React from "react";
+
+
 import useCursorOutside from "../../Custom-Hooks/useCursorOutside";
 
 interface CancelReservationPopUpProps {
@@ -6,14 +18,17 @@ interface CancelReservationPopUpProps {
 }
 
 const CancelReservationPopUp: React.FC<CancelReservationPopUpProps> = ({ setShowCancelReservationPopUp }) => {
-	const initialRef = useRef<HTMLDivElement>(null);
+
 	const mainRef = useCursorOutside(() => {
 		setShowCancelReservationPopUp((prev) => (prev = !prev));
-	}, initialRef);
+	});
 
 	return (
-		<div ref={mainRef} className="absolute h-12 w-32 rounded-md shadow-lg" style={{ backgroundColor: "#ffff", right: "1rem", top: "0.2rem" }}>
+		<div ref={mainRef} className="absolute h-12 w-32 rounded-md shadow-lg" style={{ backgroundColor: "#ffff", right: 
+		"1rem", top: "0.2rem" }}>
+			
 			<button className="text-sm m-1 hover:bg-gray-100 active:bg-gray-200 rounded-md">Cancel Reservation</button>
+
 		</div>
 	);
 };
