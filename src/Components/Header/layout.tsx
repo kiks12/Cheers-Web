5f5d38e3949f010e5ee4461ad849e46658760255
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 21:17:31 +0800

    header layout refactored

--- src/Components/Header/layout.tsx
+++ src/Components/Header/layout.tsx
@@ -1,9 +1,25 @@
+
+/*
+
+Cheers - Header Layout
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
 import React from "react";
-import { useActivePage } from "../../Custom-Hooks/useActivePage";
+
+
 import Navhar from "./Navbar/navbar";
 
+
+import { useActivePage } from "../../Custom-Hooks/useActivePage";
+
 const Layout: React.FC = ({ children }) => {
+
 	const [activePage, setActivePage] = useActivePage();
+
+
 	return (
 		<div>
 			<Navhar activePage={activePage} setActivePage={setActivePage} />
