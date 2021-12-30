45bdbcc411f40fa0ee28280ba44b2d88301a1a05
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 22:37:39 +0800

    profile settings page refactored

--- pages/profile/settings/index.tsx
+++ pages/profile/settings/index.tsx
@@ -1,15 +1,28 @@
+
+/*
+
+Cheers - Profile Settings Page
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
 import React from "react";
 import Head from "next/head";
 
+
 interface AccountSettingsProps {}
 
+
 const AccountSettings: React.FC<AccountSettingsProps> = () => {
+
+
   return (
     <>
       <Head>
         <title>Settings</title>
       </Head>
-      {/* <Navbar activePage="" /> */}
+
       <main className='py-16 container mx-auto'>
         <h1>Settings</h1>
       </main>
