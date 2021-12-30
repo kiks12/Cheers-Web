b484d6ec406c8b233b375fb7d76b1aa5d48616d7
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 22:45:15 +0800

    index page refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,40 @@
+
+/*
+
+Cheers - Index Page
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
+
 import Head from "next/head";
+import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+
+
 import Featured from "../src/Components/Feed/Featured";
 import Feed from "../src/Components/Feed/Feed";
-import { NextPage } from "next";
-import useTopOfPage from "../src/Custom-Hooks/useTopOfPage";
 import ScrollToTopButton from "../src/Components/ScrollToTop/ScrollToTopButton";
-import { useSession } from "next-auth/react";
 import Footer from "../src/Components/Footer/Footer";
 
+
+import useTopOfPage from "../src/Custom-Hooks/useTopOfPage";
+
+
 const Home: NextPage = () => {
+
   const topOfPage = useTopOfPage();
+
   const { status } = useSession();
+
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
+
       {status !== "loading" && (
         <>
           <main className='lg:container md:mx-5 sm:mx-5 lg:mx-auto h-full py-16 flex flex-col items-center'>
@@ -25,6 +45,7 @@ const Home: NextPage = () => {
           <Footer />
         </>
       )}
+      
     </>
   );
 };
