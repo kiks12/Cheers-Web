ea6c47eaf7bc92a5622a3f53a43cfbea7699b292
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-30 22:01:29 +0800

    review component refactors

--- src/Components/SingleBarPage/Review.tsx
+++ src/Components/SingleBarPage/Review.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+
+/*
+
+Cheers - Review Component
+Last Updated: Dec. 30, 2021
+Tolentino, Francis James S.
+
+*/
+
 import { BsThreeDots } from "react-icons/bs";
 import { MdStar } from "react-icons/md";
 
+
+import React from "react";
+
+
 const Review: React.FC = () => {
+	
 	return (
 		<div className="w-full h-auto break-words p-3 border mt-3 rounded-md wrap">
 			<div className="w-full flex justify-between">
