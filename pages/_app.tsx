d431bfadc0e7e74060450ff7a3c9109a74e615ac
kiks12 <francistolentino1107@gmail.com>
2021-12-31 14:35:11 +0800

    _app refactors - spaces added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@
 
 /*
 
-Cheers - _app main page
-Last Updated: Dec. 30, 2021
+Cheers - _app main component
+Last Updated: Dec. 31, 2021
 Tolentino, Francis James S.
 
 */
@@ -11,6 +11,10 @@ Tolentino, Francis James S.
 
 import "../styles/out.css";
 import "../styles/globals.css";
+
+
+import type { AppProps } from "next/app";
+
 
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
@@ -23,41 +27,39 @@ import Layout from "../src/Components/Header/layout";
 import { ActivePageProvider } from "../src/Custom-Hooks/useActivePage";
 
 
-interface myAppProps {
-  Component: any;
-  pageProps: any;
-}
-
-
-const MyApp = ({Component, pageProps: { session, ...pageProps }}: myAppProps) => {
+const MyApp = ( { Component, pageProps: { session, ...pageProps } }: AppProps) => {
   
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <>
-      <Head>
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-        />
-      </Head>
+    return (
+        <>
+          <Head>
+            <link
+              rel='stylesheet'
+              href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+            />
+          </Head>
 
-      <SessionProvider session={session}>
+          <SessionProvider session={session}>
 
-        <ActivePageProvider>
-          {router.pathname === "/login" ? (
-            <Component {...pageProps} />
-          ) : (
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          )}
-        </ActivePageProvider>
-        
-      </SessionProvider>
+            <ActivePageProvider>
 
-    </>
-  );
+              {
+                router.pathname === "/login" ? (
+                  <Component {...pageProps} />
+                ) : (
+                  <Layout>
+                    <Component {...pageProps} />
+                  </Layout>
+                )
+              }
+
+            </ActivePageProvider>
+            
+          </SessionProvider>
+        </>
+    );
 };
+
 
 export default MyApp;
