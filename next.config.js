340ca5bbb05d6d2bb42bfdf455a98d8e3df4099b
Francis James Tolentino <francistolentino1107@gmail.com>
2021-12-31 11:19:50 +0800

    webpack configuration removed

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ module.exports = {
   images: {
     domains: ["lh3.googleusercontent.com", "platform-lookaside.fbsbx.com"],
   },
-  webpack(config, { dev }) {
-    if (dev) {
-      config.devtool = "cheap-module-source-map";
-    }
-    return config;
-  },
+  // webpack(config, { dev }) {
+  //   if (dev) {
+  //     config.devtool = "cheap-module-source-map";
+  //   }
+  //   return config;
+  // },
 };
