c1129b60f7365ff9cfcb46dc18f69d31e4b4e66a
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-03 23:17:41 +0800

    layout filter component flicker fixed (added margin top of 10)

--- src/Components/Header/layout.tsx
+++ src/Components/Header/layout.tsx
@@ -54,80 +54,79 @@ const Layout: React.FC = ({ children }) => {
 						</div>
 					</div>
 
-				<div
-				className="w-screen sticky items-center justify-center lg:flex md:hidden sm:hidden z-20"
-				style={{ top: "3rem" }}
-				>
-				
-					<div className="flex lg:items-end mt-1 pb-5 pt-2 lg:container md:container sm:mx-5 lg:flex-row lg:justify-between md:flex-col md:w-full md:items-center md:justify-center sm:flex-col sm:w-full sm:items-center sm:justify-center">
-						
-						<div className="flex flex-1 lg:flex-row md:flex-col md:w-full sm:flex-col sm:w-full justify-between">
+					<div
+					className="w-screen items-center justify-center lg:flex md:hidden sm:hidden z-20 mt-10"
+					>
+					
+						<div className="flex lg:items-end mt-1 pb-5 pt-2 lg:container md:container sm:mx-5 lg:flex-row lg:justify-between md:flex-col md:w-full md:items-center md:justify-center sm:flex-col sm:w-full sm:items-center sm:justify-center">
+							
+							<div className="flex flex-1 lg:flex-row md:flex-col md:w-full sm:flex-col sm:w-full justify-between">
 
-									<div className="flex lg:flex-row lg:items-end lg:justify-between lg:w-3/4 md:flex-col md:w-full sm:flex-col sm:w-full">
-										<div className="flex-1 lg:mr-5 md:mr-0 sm:mr-0 flex flex-col justify-between h-full">
-											<p className="font-semibold mb-2">Type</p>
-											<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
-												<option>Both</option>
-												<option>Bars</option>
-												<option>Clubs</option>
-											</select>
+										<div className="flex lg:flex-row lg:items-end lg:justify-between lg:w-3/4 md:flex-col md:w-full sm:flex-col sm:w-full">
+											<div className="flex-1 lg:mr-5 md:mr-0 sm:mr-0 flex flex-col justify-between h-full">
+												<p className="font-semibold mb-2">Type</p>
+												<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
+													<option>Both</option>
+													<option>Bars</option>
+													<option>Clubs</option>
+												</select>
+											</div>
+
+											<div className="flex-1 lg:mr-5 md:mr-0 lg:mt-0 md:mt-5 sm:mt-5 flex flex-col justify-between h-full">
+												<p className="font-semibold mb-2">Reviews</p>
+												<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
+													<option>All</option>
+													<option>5 Stars</option>
+													<option>4 Stars</option>
+													<option>3 Stars</option>
+													<option>2 Stars</option>
+													<option>1 Star</option>
+												</select>
+											</div>
+
+											<div className="flex-1 lg:mr-5 md:mr-0 lg:mt-0 md:mt-5 sm:mt-5 flex flex-col justify-between h-full">
+												<p className="font-semibold mb-2">Location</p>
+												<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
+													<option>Near Me</option>
+													<option>Bars</option>
+													<option>Clubs</option>
+												</select>
+											</div>
+
+											<div className="flex-1 lg:mt-0 md:mt-5 sm:mt-5">
+												<p className="font-semibold mb-2">Price Range</p>
+												<p className="text-sm">P100.00</p>
+												<input type="range" className="w-full slider" min="50" max="1000" />
+											</div>
 										</div>
 
-										<div className="flex-1 lg:mr-5 md:mr-0 lg:mt-0 md:mt-5 sm:mt-5 flex flex-col justify-between h-full">
-											<p className="font-semibold mb-2">Reviews</p>
-											<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
-												<option>All</option>
-												<option>5 Stars</option>
-												<option>4 Stars</option>
-												<option>3 Stars</option>
-												<option>2 Stars</option>
-												<option>1 Star</option>
-											</select>
+										<div className="lg:w-1/4 lg:pl-5">
+											<div className="flex lg:w-full md:w-full sm:w-full items-end justify-end md:mt-10 sm:mt-10">
+												<button className="w-full bg-black text-white font-light h-8 rounded-md hover:bg-gray-700 transition-all text-sm">Apply</button>
+											</div>
+
+											<div className="flex items-center justify-center lg:hidden">
+												<button
+													className="w-full border border-red-400 text-red-400 font-light h-8 rounded-md hover:bg-red-400 hover:text-white mt-2 transition-all text-sm"
+												>
+													Cancel
+												</button>
+											</div>
 										</div>
 
-										<div className="flex-1 lg:mr-5 md:mr-0 lg:mt-0 md:mt-5 sm:mt-5 flex flex-col justify-between h-full">
-											<p className="font-semibold mb-2">Location</p>
-											<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
-												<option>Near Me</option>
-												<option>Bars</option>
-												<option>Clubs</option>
-											</select>
-										</div>
-
-										<div className="flex-1 lg:mt-0 md:mt-5 sm:mt-5">
-											<p className="font-semibold mb-2">Price Range</p>
-											<p className="text-sm">P100.00</p>
-											<input type="range" className="w-full slider" min="50" max="1000" />
-										</div>
 									</div>
 
-									<div className="lg:w-1/4 lg:pl-5">
-										<div className="flex lg:w-full md:w-full sm:w-full items-end justify-end md:mt-10 sm:mt-10">
-											<button className="w-full bg-black text-white font-light h-8 rounded-md hover:bg-gray-700 transition-all text-sm">Apply</button>
-										</div>
-
-										<div className="flex items-center justify-center lg:hidden">
-											<button
-												className="w-full border border-red-400 text-red-400 font-light h-8 rounded-md hover:bg-red-400 hover:text-white mt-2 transition-all text-sm"
-											>
-												Cancel
-											</button>
-										</div>
-									</div>
-
-								</div>
+						</div>
 
 					</div>
 
-				</div>
-
-				<div className="w-full flex items-center justify-center lg:hidden md:block">
-					<button
-						className="border border-black w-full py-2 px-5 rounded-md hover:bg-black hover:text-white transition-all"
-					>
-						Filters
-					</button>
-				</div>
+					<div className="w-full flex items-center justify-center lg:hidden md:block mt-10">
+						<button
+							className="border border-black w-full py-2 px-5 rounded-md hover:bg-black hover:text-white transition-all"
+						>
+							Filters
+						</button>
+					</div>
 				</main>
 			</>
 		)
