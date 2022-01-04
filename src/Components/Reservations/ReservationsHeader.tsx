af7b2fee74cc35436587a7db0336b7e2ae5a364f
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-04 23:11:23 +0800

    reservations header sort by and search button rendering fixed

--- src/Components/Reservations/ReservationsHeader.tsx
+++ src/Components/Reservations/ReservationsHeader.tsx
@@ -2,7 +2,7 @@
 /*
 
 Cheers - Reservations Header Component
-Last Updated: Dec. 30, 2021
+Last Updated: Jan. 4, 2022
 Tolentino, Francis James S.
 
 */
@@ -35,8 +35,8 @@ const ReservationsHeader: React.FC<ReservationsHeaderProps> = () => {
 
 			<h1 className="font-semibold text-xl mb-5">Reservations</h1>
 
-			<div className="flex justify-between lg:flex-row md:flex-ro`w sm:flex-col">
-				<div className="flex lg:flex-row md:flex-row sm:flex-col lg:mr-5 md:mr-5 sm:mr-0 lg:items-center md:items-center">
+			<div className="flex justify-between lg:flex-row md:flex-row sm:flex-col">
+				<div className="lg:flex md:flex lg:flex-row md:flex-row sm:flex-col lg:mr-5 md:mr-5 sm:mr-0 lg:items-center md:items-center sm:hidden">
 					<p className="mr-3 font-light text-sm">Sort By:</p>
 					<select
 						className="bg-white border rounded-md py-1 px-3 text-sm"
@@ -55,13 +55,28 @@ const ReservationsHeader: React.FC<ReservationsHeaderProps> = () => {
 						<input type="date" className="bg-white border text-sm py-1 px-2 rounded-md" />
 					</div>
 				)}
-				<div className="lg:flex-1 md:flex-1 sm:w-full flex justify-end lg:mt-0 md:mt-0 sm:mt-3">
+				<div className="lg:flex-1 md:flex-1 sm:w-full flex lg:flex-row md:flex-row sm:flex-col justify-end">
 					<input className="bg-white border rounded-md lg:w-72 md:w-72 sm:w-full p-1 mr-3 text-sm py-1" placeholder="Search Reservation" />
 
-					<button className="bg-black hover:bg-gray-700 active:bg-gray-400 text-white px-6 py-1 text-sm rounded-md ">
+					<button className="bg-black hover:bg-gray-700 active:bg-gray-400 text-white px-6 py-1 border border-black text-sm rounded-md ;lg:mt-0 md:mt-0 sm:mt-2 ">
 						Search
 					</button>
 				</div>
+
+				{/* small screen */}
+				<div className="flex flex-col mt-2 lg:hidden md:hidden">
+					<p className="mr-3 font-light text-sm">Sort By:</p>
+					<select
+						className="bg-white border rounded-md py-1 px-3 text-sm"
+						value={sortBy}
+						onChange={handleSortByChange}
+					>
+						<option value={ALPHABETICAL}>Alphabetical</option>
+						<option value={LATEST}>Latest</option>
+						<option value={SPECIFIC_DATE}>Specific Date</option>
+					</select>
+				</div>
+
 			</div>
 		</div>
 	);
