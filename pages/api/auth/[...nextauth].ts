15b740bd5f19096c611cdf6fbea7e850c3aabc1e
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-06 23:09:12 +0800

    facebook client id and secret put into env file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@
 /*
 
 Cheers - Next auth Middleware Configuration
-Last Updated: Dec. 30, 2021
+Last Updated: Jan. 6, 2022
 Tolentino, Francis James S.
 
 */
@@ -17,6 +17,8 @@ import FacebookProvider from "next-auth/providers/facebook";
 
 const GOOGLE_CLIENT_ID : string = process.env.GOOGLE_CLIENT_ID as string;
 const GOOGLE_CLIENT_SECRET : string = process.env.GOOGLE_CLIENT_SECRET as string;
+const FACEBOOK_CLIENT_ID : string = process.env.FACEBOOK_CLIENT_ID as string;
+const FACEBOOK_CLIENT_SECRET : string = process.env.FACEBOOK_CLIENT_SECRET as string;
 
 
 export default NextAuth({
@@ -30,8 +32,8 @@ export default NextAuth({
 		}),
 
 		FacebookProvider({
-			clientId: "1295794440886790",
-			clientSecret: "1db5ab54b8f0f5f6cdec953a7f943ce7",
+			clientId: FACEBOOK_CLIENT_ID,
+			clientSecret: FACEBOOK_CLIENT_SECRET,
 		}),
 	],
 
@@ -39,5 +41,6 @@ export default NextAuth({
 		session: async ({ session }) => {
 			return Promise.resolve(session);
 		},
-	},
+	}
+	
 });
