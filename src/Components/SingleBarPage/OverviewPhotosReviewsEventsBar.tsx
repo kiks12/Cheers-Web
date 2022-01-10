4ceda92beeffa43b9798f58fbbafc95203922e67
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-10 23:42:48 +0800

    overview photos reviews events nav bar responsiveness better

--- src/Components/SingleBarPage/OverviewPhotosReviewsEventsBar.tsx
+++ src/Components/SingleBarPage/OverviewPhotosReviewsEventsBar.tsx
@@ -2,23 +2,73 @@
 /*
 
 Cheers - One Bar Page OPRE Bar Component
-Last Updated: Dec. 30, 2021
+Last Updated: Jan. 10, 2022 
 Tolentino, Francis James S.
 
 */
 
-import React from "react";
+
+import React, { useEffect, useRef } from "react";
 import Link from "next/dist/client/link";
+
 
 interface OPREBarProps {
 	active: string;
 }
 
+
 const OverviewPhotosReviewsEventsBar: React.FC<OPREBarProps> = ({ active }) => {
+
+	const navbarRef = useRef<HTMLElement | null>(null);
+	const ulRef = useRef<HTMLUListElement | null>(null);
+
+
+	const uListLeftIsLessThanNavbarLeft = () => {
+		if (navbarRef.current && ulRef.current){
+			return navbarRef.current?.getBoundingClientRect().left > ulRef.current?.getBoundingClientRect().left;
+		}
+
+		return false;
+	}
+
+
+	const uListRightIsGreaterThanNavbarRight = () => {
+		if (navbarRef.current && ulRef.current){
+			return ulRef.current?.getBoundingClientRect().right > navbarRef.current?.getBoundingClientRect().right;
+		}
+
+		return false;
+	}
+
+
+	const navbarAndUListRelationHandler = () => {
+		if (navbarRef.current && ulRef.current){
+			if (uListLeftIsLessThanNavbarLeft() && uListRightIsGreaterThanNavbarRight()){
+				ulRef.current.classList.add("sm:left-0");
+				return;
+			} 
+			ulRef.current.classList.remove("sm:left-0");
+			console.log("removing");
+		}	
+	}
+
+
+	useEffect(() => {
+		navbarAndUListRelationHandler();
+	}, [navbarRef, ulRef]);
+
+
+	useEffect(() => {
+		window.addEventListener("resize", navbarAndUListRelationHandler);
+
+		return () => window.removeEventListener("resize", navbarAndUListRelationHandler);
+	}, [navbarRef, ulRef]);
+
+
 	return (
-		<nav className="w-full h-8 border-t border-b mt-2 flex items-center justify-center  sm:relative">
+		<nav ref={navbarRef} className="w-full h-8 border-t border-b mt-2 flex justify-center sm:overflow-x-auto sm:relative">
 			
-      <ul className="flex h-full lg:w-1/2 md:w-2/3 items-center mx-auto justify-center md:static sm:absolute sm:top-0 sm:left-0 sm:bottom-0">
+			<ul ref={ulRef} className="flex h-full lg:w-1/2 md:w-2/3 items-center md:container md:mx-auto md:static sm:absolute sm:top-0 sm:bottom-0 justify-center">
 
 				<Link href="/Bar-name/0/">
 					{active === "overview" ? (
