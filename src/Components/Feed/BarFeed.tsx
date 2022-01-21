e01d510611cf632086669449206a752e53ee07e2
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-21 16:51:57 +0800

    bar feed redesign

--- src/Components/Feed/BarFeed.tsx
+++ src/Components/Feed/BarFeed.tsx
@@ -24,13 +24,17 @@ const BarFeed: React.FC = () => {
 
 		<Link href={`/${BARNAME}/${IS_FEATURED ? 1 : 0}`}>
 			
-			<div className="mt-2 h-28 bg-transparent border hover:border-gray-400 p-2 flex justify-between w-full cursor-pointer relative">
+			<div className="mt-2 h-28 bg-transparent border hover:border-gray-400 flex justify-between w-full cursor-pointer relative">
 
-				<div className="flex flex-col flex-1 pr-2">
+				<div className="w-48 h-full text-sm bg-black text-white flex items-center justify-center lg:flex md:flex sm:hidden z-10">
+					image
+				</div>
+
+				<div className="flex flex-col flex-1 p-2 justify-between wrap">
 
 					<div className=" pb-2">
 						<div className="flex items-center">
-							<p className="font-semibold text-lg mr-2">Bar Name</p>
+							<p className="mr-2">Bar Name</p>
 							<div className="flex">
 								<MdStar color="#FFE769" size={20} />
 								<MdStar color="#FFE769" size={20} />
@@ -39,20 +43,19 @@ const BarFeed: React.FC = () => {
 								<MdStar color="#FFE769" size={20} />
 							</div>
 						</div>
-						<p className="text-sm">Bar</p>
+						<p className="text-sm">Type: Bar</p>
 					</div>
 
-					<div className="flex items-center h-8 py-1 font-light text-black pl-2 pb-2 absolute bottom-0 left-0 right-0 z-0 rounded-b-md">
-						<MdOutlineLocationOn size={18} color="#2d2d2d" />
-						<p className="ml-1 text-sm">1.2 km</p>
-						<p className="ml-10 text-sm">200php - 500php</p>
+					<div className="flex flex-col justify-start text-gray-400">
+						<div className="flex flex-row items-center">
+							<MdOutlineLocationOn size={17} color="gray" />
+							<p className="ml-1 text-sm">1.2 km</p>
+						</div>
+						<p className="ml-2 text-sm">200php - 500php</p>
 					</div>
 					
 				</div>
-
-				<div className="w-48 h-full text-sm bg-black rounded-sm text-white flex items-center justify-center lg:flex md:flex sm:hidden z-10">
-					image
-				</div>
+				
 
 			</div>
 
