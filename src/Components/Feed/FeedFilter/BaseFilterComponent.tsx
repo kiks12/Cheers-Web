f6480afb380791eb4ff1acfd1ab898d0ae879ac2
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-21 15:15:07 +0800

    feed filter component rounded corners removed

--- src/Components/Feed/FeedFilter/BaseFilterComponent.tsx
+++ src/Components/Feed/FeedFilter/BaseFilterComponent.tsx
@@ -2,7 +2,7 @@
 /*
 
 Cheers - Base Filter Component (Filter component that handles the ui button rendering)
-Last Updated: Jan. 3, 2022
+Last Updated: Jan. 21, 2022
 Tolentino, Francis James S.
 
 */
@@ -16,6 +16,7 @@ interface FilterComponentProps {
 	setShowFilterModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+
 const FilterComponent: React.FC<FilterComponentProps> = ({ setShowFilterModal }) => {
 
 	
@@ -25,7 +26,7 @@ const FilterComponent: React.FC<FilterComponentProps> = ({ setShowFilterModal })
 			<div className="flex lg:flex-row lg:items-end lg:justify-between lg:w-3/4 md:flex-col md:w-full sm:flex-col sm:w-full">
 				<div className="flex-1 lg:mr-5 md:mr-0 sm:mr-0 flex flex-col justify-between h-full">
 					<p className="font-semibold mb-2">Type</p>
-					<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
+					<select className="w-full h-8 bg-white border px-2 text-sm">
 						<option>Both</option>
 						<option>Bars</option>
 						<option>Clubs</option>
@@ -34,7 +35,7 @@ const FilterComponent: React.FC<FilterComponentProps> = ({ setShowFilterModal })
 
 				<div className="flex-1 lg:mr-5 md:mr-0 lg:mt-0 md:mt-5 sm:mt-5 flex flex-col justify-between h-full">
 					<p className="font-semibold mb-2">Reviews</p>
-					<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
+					<select className="w-full h-8 bg-white border px-2 text-sm">
 						<option>All</option>
 						<option>5 Stars</option>
 						<option>4 Stars</option>
@@ -46,7 +47,7 @@ const FilterComponent: React.FC<FilterComponentProps> = ({ setShowFilterModal })
 
 				<div className="flex-1 lg:mr-5 md:mr-0 lg:mt-0 md:mt-5 sm:mt-5 flex flex-col justify-between h-full">
 					<p className="font-semibold mb-2">Location</p>
-					<select className="w-full h-8 bg-white border rounded-md px-2 text-sm">
+					<select className="w-full h-8 bg-white border px-2 text-sm">
 						<option>Near Me</option>
 						<option>Bars</option>
 						<option>Clubs</option>
@@ -62,12 +63,12 @@ const FilterComponent: React.FC<FilterComponentProps> = ({ setShowFilterModal })
 
 			<div className="lg:w-1/4 lg:pl-5">
 				<div className="flex lg:w-full md:w-full sm:w-full items-end justify-end md:mt-10 sm:mt-10">
-					<button className="w-full bg-black text-white font-light h-8 rounded-md hover:bg-gray-700 transition-all text-sm">Apply</button>
+					<button className="w-full bg-black text-white font-light h-8 hover:bg-gray-700 transition-all text-sm">Apply</button>
 				</div>
 
 				<div className="flex items-center justify-center lg:hidden">
 					<button
-						className="w-full border border-red-400 text-red-400 font-light h-8 rounded-md hover:bg-red-400 hover:text-white mt-2 transition-all text-sm"
+						className="w-full border border-red-400 text-red-400 font-light h-8 hover:bg-red-400 hover:text-white mt-2 transition-all text-sm"
 						onClick={() => setShowFilterModal(false)}
 					>
 						Cancel
@@ -78,5 +79,6 @@ const FilterComponent: React.FC<FilterComponentProps> = ({ setShowFilterModal })
 		</div>
 	);
 };
+
 
 export default FilterComponent;
