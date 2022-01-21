920f2869143a9370e33c7e8edfa0562e2bec8aba
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-21 16:30:48 +0800

    reviews grid implementation

--- pages/[bar]/[isFeatured]/reviews.tsx
+++ pages/[bar]/[isFeatured]/reviews.tsx
@@ -2,7 +2,7 @@
 /*
 
 Cheers - Single Bar Page Review Page
-Last Updated: Dec. 30, 2021
+Last Updated: Jan. 21, 2022
 Tolentino, Francis James S.
 
 */
@@ -14,6 +14,8 @@ import { useRouter } from "next/dist/client/router";
 import Head from "next/dist/shared/lib/head";
 
 
+import Feed from "../../../src/Components/Feed/Feed";
+import BarPageSkeleton from "../../../src/Components/SingleBarPage/BarPageSkeleton";
 import PageBar from "../../../src/Components/SingleBarPage/PageHeader";
 import Review from "../../../src/Components/SingleBarPage/Review";
 
@@ -41,19 +43,25 @@ const Bar = () => {
         <title>{bar}</title>
       </Head>
 
-      <main className='md:mx-5 sm:mx-5 lg:container lg:mx-auto h-full py-16 flex flex-col items-center'>
-        
-        <PageBar barName={bar} isFeatured={true} active='reviews' />
 
-        <div className='w-full mt-5 h-16 border rounded-md flex items-center p-3'>
-          <input
-            className='h-full bg-white border p-2 lg:w-1/4 md:w-1/3 sm:w-full rounded-full text-sm'
-            placeholder='Add a review'
-          />
-        </div>
+      <BarPageSkeleton>
+          <div className="col-span-2">
+              <PageBar barName={bar} isFeatured={true} active='reviews' />
 
-        {dummyReviews && dummyReviews.map((review) => <Review key={review} />)}
-      </main>
+              <div className='w-full mt-5 h-16 border rounded-md flex items-center p-3'>
+                  <input
+                    className='h-full bg-white border p-2 lg:w-1/4 md:w-1/3 sm:w-full rounded-full text-sm'
+                    placeholder='Add a review'
+                  />
+              </div>
+
+              {dummyReviews && dummyReviews.map((review) => <Review key={review} />)}
+          </div>
+
+          <div>
+              <Feed />
+          </div>
+      </BarPageSkeleton>
     </>
   );
 };
