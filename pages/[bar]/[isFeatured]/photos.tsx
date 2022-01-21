157588596ccb6a68a45a036fa17904e138e9c475
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-21 16:26:38 +0800

    photos grid implementation

--- pages/[bar]/[isFeatured]/photos.tsx
+++ pages/[bar]/[isFeatured]/photos.tsx
@@ -2,15 +2,18 @@
 /*
 
 Cheers - Single Bar Page Photos Component
-Last Updated: Dec. 30, 2021
+Last Updated: Jan. 21, 2022
 Tolentino, Francis James S.
 
 */
+
 
 import { NextRouter, useRouter } from "next/dist/client/router";
 import Head from "next/dist/shared/lib/head";
 
 
+import BarPageSkeleton from "../../../src/Components/SingleBarPage/BarPageSkeleton";
+import Feed from "../../../src/Components/Feed/Feed";
 import PageBar from "../../../src/Components/SingleBarPage/PageHeader";
 
 
@@ -40,28 +43,31 @@ const Photos = () => {
       </Head>
 
 
-      <main className='md:mx-5 sm:mx-5 lg:container lg:mx-auto h-full py-16 flex flex-col items-center'>
-        
-        <PageBar barName={bar} isFeatured={true} active='photos' />
+      <BarPageSkeleton>
+          <div className="col-span-2">
+              <PageBar barName={bar} isFeatured={true} active="photos" />
 
-        <div className='mt-5 flex w-full flex-wrap'>
-          {dummyImages ? (
-            dummyImages.map((image) => {
-              return (
-                <div
-                  key={image}
-                  className='lg:w-1/4-m-1 md:w-1/3-m-1 sm:w-1/2-m-1 mr-1 mt-1 bg-gray-300 lg:h-56 md:h-64 sm:h-36 cursor-pointer hover:shadow-md flex items-center justify-center rounded-lg border border-transparent hover:border-white'>
-                  {image}
-                </div>
-              );
-            })
-          ) : (
-            <p>No Photos to show</p>
-          )}
-        </div>
+              <div className='mt-5 flex w-full flex-wrap'>
+                {dummyImages ? (
+                  dummyImages.map((image) => {
+                    return (
+                      <div
+                        key={image}
+                        className='lg:w-1/4-m-1 md:w-1/3-m-1 sm:w-1/2-m-1 mr-1 mt-1 bg-gray-300 lg:h-56 md:h-64 sm:h-36 cursor-pointer hover:shadow-md flex items-center justify-center rounded-lg border border-transparent hover:border-white'>
+                        {image}
+                      </div>
+                    );
+                  })
+                ) : (
+                  <p>No Photos to show</p>
+                )}
+              </div>
+          </div>
 
-      </main>
-
+          <div>
+              <Feed/>
+          </div>
+      </BarPageSkeleton>
 
     </>
   );
