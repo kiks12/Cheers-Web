becfbee727b97c0a191103bd0609c2744e3ad460
kiks12 <francistolentino1107@gmail.com>
2022-01-27 20:17:08 +0800

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,90 @@
+
+/*
+
+Cheers - Tailwind Configuration
+Last Updated: Jan. 27, 2022
+Tolentino, Francis James S.
+
+*/
+
 const colors = require("tailwindcss/colors");
 
 module.exports = {
+
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/Components/**/*.{js, ts, jsx, tsx}'],
+
   darkMode: false, // or 'media' or 'class'
+
   theme: {
+
     colors:{
+
       transparent: "transparent",
+
       black:{
         DEFAULT: "#2D2D2D"
       },
+      
       white:{
         light: "#FFFFFF",
         DEFAULT: "#F8F8F8"
       },
+      
       gray: colors.gray,
       yellow: colors.yellow,
       red: colors.red,
       indigo: colors.indigo
     },
+
     extend: {
+
       width: {
         'full-hover': '105%',
         '1/4-m-1': '24.5%',
         '1/3-m-1': '32.5%',
         '1/2-m-1': '48.5%',
       },
+
       height: {
         'full-hover': '105%',
         '1/4-m-1': '24%',
         '1/3-m-1': '32.2%',
         '1/2-m-1': '48.2%',
       },
+
     },
+
     screens:{
       'sm': '300px',
       'md': '768px',
       'lg': '1280px',
-    }
+    },
+
+    container: {
+
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+
+    },
   },
+
   variants: {
+
     extend: {
       backgroundColor: ['active'],
     },
+
     width: ["responsive", "hover", "focus"],
+
     height: ["responsive", "hover", "focus"]
+
   },
+
   plugins: [],
+
 }
